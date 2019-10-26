a15dcb43192e279e0fe7fd43fe6fe34252e82c4b
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2019-10-26 11:59:01 +0900

    多次元コレクション機能。
    Eclipseでコンパイルした時にエラーが発生しないよう修正。

--- corelib/elemental-collection/src/main/java/cherry/elemental/util/MdMaps.java
+++ corelib/elemental-collection/src/main/java/cherry/elemental/util/MdMaps.java
@@ -41,47 +41,47 @@ public class MdMaps {
 	public static final Creator TREEMAP = TreeMap::new;
 
 	public static <X1, R> Map<X1, R> defaulted1dMap(Creator creator, Transformer<? super X1, ? extends R> transformer) {
-		return DefaultedMap.<X1, R> defaultedMap(creator.<X1, R> create(), x1 -> transformer.transform(x1));
+		return DefaultedMap.<X1, R> defaultedMap(creator.<X1, R> create(),
+				(Transformer<? super X1, ? extends R>) x1 -> transformer.transform(x1));
 	}
 
 	public static <X1, X2, R> Map<X1, Map<X2, R>> defaulted2dMap(Creator creator,
 			Transformer<Tuple2<? super X1, ? super X2>, ? extends R> transformer) {
-		return defaulted1dMap(
-				creator,
-				x1 -> DefaultedMap.<X2, R> defaultedMap(creator.<X2, R> create(),
-						x2 -> transformer.transform(Tuples.of(x1, x2))));
+		return defaulted1dMap(creator, x1 -> DefaultedMap.<X2, R> defaultedMap(creator.<X2, R> create(),
+				(Transformer<? super X2, ? extends R>) x2 -> transformer.transform(Tuples.of(x1, x2))));
 	}
 
 	public static <X1, X2, X3, R> Map<X1, Map<X2, Map<X3, R>>> defaulted3dMap(Creator creator,
 			Transformer<Tuple3<? super X1, ? super X2, ? super X3>, ? extends R> transformer) {
-		return defaulted2dMap(
-				creator,
+		return defaulted2dMap(creator,
 				p -> DefaultedMap.<X3, R> defaultedMap(creator.<X3, R> create(),
-						x3 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), x3))));
+						(Transformer<? super X3, ? extends R>) x3 -> transformer
+								.transform(Tuples.of(p.getX1(), p.getX2(), x3))));
 	}
 
 	public static <X1, X2, X3, X4, R> Map<X1, Map<X2, Map<X3, Map<X4, R>>>> defaulted4dMap(Creator creator,
 			Transformer<Tuple4<? super X1, ? super X2, ? super X3, ? super X4>, ? extends R> transformer) {
-		return defaulted3dMap(
-				creator,
+		return defaulted3dMap(creator,
 				p -> DefaultedMap.<X4, R> defaultedMap(creator.<X4, R> create(),
-						x4 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), x4))));
+						(Transformer<? super X4, ? extends R>) x4 -> transformer
+								.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), x4))));
 	}
 
-	public static <X1, X2, X3, X4, X5, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, R>>>>> defaulted5dMap(
-			Creator creator,
+	public static <X1, X2, X3, X4, X5, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, R>>>>> defaulted5dMap(Creator creator,
 			Transformer<Tuple5<? super X1, ? super X2, ? super X3, ? super X4, ? super X5>, ? extends R> transformer) {
-		return defaulted4dMap(
-				creator,
+		return defaulted4dMap(creator,
 				p -> DefaultedMap.<X5, R> defaultedMap(creator.<X5, R> create(),
-						x5 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), p.getX4(), x5))));
+						(Transformer<? super X5, ? extends R>) x5 -> transformer
+								.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), p.getX4(), x5))));
 	}
 
 	public static <X1, X2, X3, X4, X5, X6, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, Map<X6, R>>>>>> defaulted6dMap(
 			Creator creator,
 			Transformer<Tuple6<? super X1, ? super X2, ? super X3, ? super X4, ? super X5, ? super X6>, ? extends R> transformer) {
-		return defaulted5dMap(creator, p -> DefaultedMap.<X6, R> defaultedMap(creator.<X6, R> create(),
-				x6 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), p.getX4(), p.getX5(), x6))));
+		return defaulted5dMap(creator,
+				p -> DefaultedMap.<X6, R> defaultedMap(creator.<X6, R> create(),
+						(Transformer<? super X6, ? extends R>) x6 -> transformer
+								.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), p.getX4(), p.getX5(), x6))));
 	}
 
 	public static <X1, R> Map<X1, R> defaulted1dMap(Creator creator, Factory<R> factory) {
@@ -101,8 +101,8 @@ public class MdMaps {
 		return defaulted4dMap(creator, p -> factory.create());
 	}
 
-	public static <X1, X2, X3, X4, X5, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, R>>>>> defaulted5dMap(
-			Creator creator, Factory<R> factory) {
+	public static <X1, X2, X3, X4, X5, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, R>>>>> defaulted5dMap(Creator creator,
+			Factory<R> factory) {
 		return defaulted5dMap(creator, p -> factory.create());
 	}
 
@@ -128,8 +128,8 @@ public class MdMaps {
 		return defaulted4dMap(creator, p -> defaultValue);
 	}
 
-	public static <X1, X2, X3, X4, X5, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, R>>>>> defaulted5dMap(
-			Creator creator, R defaultValue) {
+	public static <X1, X2, X3, X4, X5, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, R>>>>> defaulted5dMap(Creator creator,
+			R defaultValue) {
 		return defaulted5dMap(creator, p -> defaultValue);
 	}
 
@@ -144,32 +144,26 @@ public class MdMaps {
 
 	public static <X1, X2, R> Map<X1, Map<X2, R>> lazy2dMap(Creator creator,
 			Transformer<Tuple2<? super X1, ? super X2>, ? extends R> transformer) {
-		return lazy1dMap(creator,
-				x1 -> LazyMap.<R, X2> lazyMap(creator.<X2, R> create(), x2 -> transformer.transform(Tuples.of(x1, x2))));
+		return lazy1dMap(creator, x1 -> LazyMap.<R, X2> lazyMap(creator.<X2, R> create(),
+				x2 -> transformer.transform(Tuples.of(x1, x2))));
 	}
 
 	public static <X1, X2, X3, R> Map<X1, Map<X2, Map<X3, R>>> lazy3dMap(Creator creator,
 			Transformer<Tuple3<? super X1, ? super X2, ? super X3>, ? extends R> transformer) {
-		return lazy2dMap(
-				creator,
-				p -> LazyMap.<R, X3> lazyMap(creator.<X3, R> create(),
-						x3 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), x3))));
+		return lazy2dMap(creator, p -> LazyMap.<R, X3> lazyMap(creator.<X3, R> create(),
+				x3 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), x3))));
 	}
 
 	public static <X1, X2, X3, X4, R> Map<X1, Map<X2, Map<X3, Map<X4, R>>>> lazy4dMap(Creator creator,
 			Transformer<Tuple4<? super X1, ? super X2, ? super X3, ? super X4>, ? extends R> transformer) {
-		return lazy3dMap(
-				creator,
-				p -> LazyMap.<R, X4> lazyMap(creator.<X4, R> create(),
-						x4 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), x4))));
+		return lazy3dMap(creator, p -> LazyMap.<R, X4> lazyMap(creator.<X4, R> create(),
+				x4 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), x4))));
 	}
 
 	public static <X1, X2, X3, X4, X5, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, R>>>>> lazy5dMap(Creator creator,
 			Transformer<Tuple5<? super X1, ? super X2, ? super X3, ? super X4, ? super X5>, ? extends R> transformer) {
-		return lazy4dMap(
-				creator,
-				p -> LazyMap.<R, X5> lazyMap(creator.<X5, R> create(),
-						x5 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), p.getX4(), x5))));
+		return lazy4dMap(creator, p -> LazyMap.<R, X5> lazyMap(creator.<X5, R> create(),
+				x5 -> transformer.transform(Tuples.of(p.getX1(), p.getX2(), p.getX3(), p.getX4(), x5))));
 	}
 
 	public static <X1, X2, X3, X4, X5, X6, R> Map<X1, Map<X2, Map<X3, Map<X4, Map<X5, Map<X6, R>>>>>> lazy6dMap(
@@ -218,8 +212,8 @@ public class MdMaps {
 		return getOpt(map, x1, x2).flatMap(m -> Optional.ofNullable(m.get(x3)));
 	}
 
-	public static <X1, X2, X3, X4, R> Optional<R> getOpt(Map<X1, Map<X2, Map<X3, Map<X4, R>>>> map, X1 x1, X2 x2,
-			X3 x3, X4 x4) {
+	public static <X1, X2, X3, X4, R> Optional<R> getOpt(Map<X1, Map<X2, Map<X3, Map<X4, R>>>> map, X1 x1, X2 x2, X3 x3,
+			X4 x4) {
 		return getOpt(map, x1, x2, x3).flatMap(m -> Optional.ofNullable(m.get(x4)));
 	}
 
