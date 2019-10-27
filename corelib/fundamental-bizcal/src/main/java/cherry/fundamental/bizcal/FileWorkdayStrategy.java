6fe5273d4363cb935241609ea3ec7cdeb989376b
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2019-10-27 22:01:54 +0900

    業務カレンダ機能：ファイルで定義する機構を作成。

---
+++ corelib/fundamental-bizcal/src/main/java/cherry/fundamental/bizcal/FileWorkdayStrategy.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2019 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.fundamental.bizcal;
+
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.MONTH;
+import static java.util.Calendar.YEAR;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.yaml.snakeyaml.Yaml;
+
+public class FileWorkdayStrategy extends AbstractWorkdayStrategy {
+
+	private final Map<String, List<Config>> configCache = new HashMap<>();
+
+	private final File basedir;
+
+	public FileWorkdayStrategy(File basedir) {
+		this.basedir = basedir;
+	}
+
+	@Override
+	protected boolean isOnSpecific(String name, LocalDate ldt) {
+		return getConfig(name).stream().map(Config::getSpecificOn).anyMatch(l -> l.contains(ldt));
+	}
+
+	@Override
+	protected boolean isOffSpecific(String name, LocalDate ldt) {
+		return getConfig(name).stream().map(Config::getSpecificOff).anyMatch(l -> l.contains(ldt));
+	}
+
+	@Override
+	protected boolean isOnRegular(String name, DayOfWeek dow) {
+		List<Set<DayOfWeek>> s = getConfig(name).stream().map(Config::getRegularOn).collect(Collectors.toList());
+		if (s.stream().allMatch(Set::isEmpty)) {
+			return true;
+		}
+		return s.stream().anyMatch(l -> l.contains(dow));
+	}
+
+	private List<Config> getConfig(String name) {
+
+		File dir = new File(basedir, name);
+		if (!dir.isDirectory()) {
+			configCache.remove(name);
+			return Collections.<Config> emptyList();
+		}
+
+		Set<String> filenames = Stream.of(dir.list()).filter(n -> new File(dir, n).isFile())
+				.collect(Collectors.toSet());
+		if (configCache.containsKey(name)) {
+			List<Config> current = configCache.get(name);
+			if (filenames.size() == current.size() && current.stream().allMatch(c -> filenames.contains(c.getFilename())
+					&& new File(dir, c.getFilename()).lastModified() == c.getLastModified())) {
+				return current;
+			}
+		}
+
+		List<Config> list = filenames.stream().sorted().map(n -> loadConfig(new File(dir, n)))
+				.collect(Collectors.toList());
+		configCache.put(name, list);
+		return list;
+	}
+
+	private Config loadConfig(File file) {
+		Yaml yaml = new Yaml();
+		try (InputStream in = new FileInputStream(file)) {
+
+			Map<String, List<?>> map = yaml.load(in);
+			List<?> ron = (List<?>) map.get("regularOn");
+			List<?> son = (List<?>) map.get("specificOn");
+			List<?> soff = (List<?>) map.get("specificOff");
+
+			if (ron == null) {
+				ron = Collections.emptyList();
+			}
+			if (son == null) {
+				son = Collections.emptyList();
+			}
+			if (soff == null) {
+				soff = Collections.emptyList();
+			}
+
+			Set<DayOfWeek> regularOn = ron.stream().filter(ObjectUtils::isNotEmpty).map(Object::toString)
+					.map(DayOfWeek::valueOf).collect(Collectors.toSet());
+			Set<LocalDate> specificOn = son.stream().filter(ObjectUtils::isNotEmpty).map(this::getLocalDate)
+					.collect(Collectors.toSet());
+			Set<LocalDate> specificOff = soff.stream().filter(ObjectUtils::isNotEmpty).map(this::getLocalDate)
+					.collect(Collectors.toSet());
+
+			return new Config(file.getName(), file.lastModified(), regularOn, specificOn, specificOff);
+		} catch (IOException ex) {
+			throw new UncheckedIOException(ex);
+		}
+	}
+
+	private LocalDate getLocalDate(Object v) {
+		if (v instanceof Date) {
+			Calendar cal = Calendar.getInstance();
+			cal.setTimeInMillis(((Date) v).getTime());
+			return LocalDate.of(cal.get(YEAR), cal.get(MONTH) + 1, cal.get(DAY_OF_MONTH));
+		}
+		if (v instanceof Number) {
+			int num = ((Number) v).intValue();
+			int dayOfMonth = num % 100; // 末尾二桁
+			int month = (num / 100) % 100; // 末尾から三桁目と四桁目
+			int year = num / 10000; // 末尾から五桁目以上
+			return LocalDate.of(year, month, dayOfMonth);
+		}
+		try {
+			return LocalDate.parse(v.toString(), DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+		} catch (DateTimeParseException ex1) {
+			try {
+				return LocalDate.parse(v.toString(), DateTimeFormatter.ofPattern("yyyyMMdd"));
+			} catch (DateTimeParseException ex2) {
+				return LocalDate.parse(v.toString());
+			}
+		}
+	}
+
+	static class Config {
+
+		private final String filename;
+
+		private final long lastModified;
+
+		private final Set<DayOfWeek> regularOn;
+
+		private final Set<LocalDate> specificOn;
+
+		private final Set<LocalDate> specificOff;
+
+		public Config(String filename, long lastModified, Set<DayOfWeek> regularOn, Set<LocalDate> specificOn,
+				Set<LocalDate> specificOff) {
+			this.filename = filename;
+			this.lastModified = lastModified;
+			this.regularOn = regularOn;
+			this.specificOn = specificOn;
+			this.specificOff = specificOff;
+		}
+
+		public String getFilename() {
+			return filename;
+		}
+
+		public long getLastModified() {
+			return lastModified;
+		}
+
+		public Set<DayOfWeek> getRegularOn() {
+			return regularOn;
+		}
+
+		public Set<LocalDate> getSpecificOn() {
+			return specificOn;
+		}
+
+		public Set<LocalDate> getSpecificOff() {
+			return specificOff;
+		}
+	}
+
+}
---
+++ corelib/fundamental-bizcal/src/test/java/cherry/fundamental/bizcal/FileWorkdayStrategyTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2019 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.fundamental.bizcal;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+public class FileWorkdayStrategyTest {
+
+	@Test
+	public void testOnRegular() {
+		// regularOn: 月火水木金。
+		FileWorkdayStrategy impl = new FileWorkdayStrategy(new File("src/test/resources/workday"));
+		// 水曜日を起点として営業日数を算出。
+		assertEquals(1, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 1)));
+		assertEquals(2, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 2)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 3)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 4)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 5)));
+		assertEquals(4, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 6)));
+		assertEquals(5, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 7)));
+		// 土曜日を起点として営業日数を算出。
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 4), LocalDate.of(2020, 1, 4)));
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 4), LocalDate.of(2020, 1, 5)));
+		assertEquals(1, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 4), LocalDate.of(2020, 1, 6)));
+		assertEquals(2, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 4), LocalDate.of(2020, 1, 7)));
+		// 水曜日を起点として翌営業日を算出。
+		assertEquals(LocalDate.of(2020, 1, 1), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 1), 1));
+		assertEquals(LocalDate.of(2020, 1, 2), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 1), 2));
+		assertEquals(LocalDate.of(2020, 1, 3), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 1), 3));
+		assertEquals(LocalDate.of(2020, 1, 6), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 1), 4));
+		assertEquals(LocalDate.of(2020, 1, 7), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 1), 5));
+		// 土曜日を起点として翌営業日を算出。
+		assertEquals(LocalDate.of(2020, 1, 4), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 4), 0));
+		assertEquals(LocalDate.of(2020, 1, 6), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 4), 1));
+		assertEquals(LocalDate.of(2020, 1, 7), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 4), 2));
+	}
+
+	@Test
+	public void testOnSpecific() {
+		// regularOn: 月火水木金。
+		// specificOn: 2019/01/11(土), 2019/01/12(日)
+		FileWorkdayStrategy impl = new FileWorkdayStrategy(new File("src/test/resources/workday"));
+		// 水曜日を起点として営業日数を算出。
+		assertEquals(1, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 8), LocalDate.of(2020, 1, 8)));
+		assertEquals(2, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 8), LocalDate.of(2020, 1, 9)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 8), LocalDate.of(2020, 1, 10)));
+		assertEquals(4, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 8), LocalDate.of(2020, 1, 11)));
+		assertEquals(5, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 8), LocalDate.of(2020, 1, 12)));
+		assertEquals(6, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 8), LocalDate.of(2020, 1, 13)));
+		assertEquals(7, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 8), LocalDate.of(2020, 1, 14)));
+		// 土曜日を起点として営業日数を算出。
+		assertEquals(1, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 11), LocalDate.of(2020, 1, 11)));
+		assertEquals(2, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 11), LocalDate.of(2020, 1, 12)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 11), LocalDate.of(2020, 1, 13)));
+		assertEquals(4, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 11), LocalDate.of(2020, 1, 14)));
+		// 水曜日を起点として翌営業日を算出。
+		assertEquals(LocalDate.of(2020, 1, 8), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 8), 1));
+		assertEquals(LocalDate.of(2020, 1, 9), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 8), 2));
+		assertEquals(LocalDate.of(2020, 1, 10), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 8), 3));
+		assertEquals(LocalDate.of(2020, 1, 11), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 8), 4));
+		assertEquals(LocalDate.of(2020, 1, 12), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 8), 5));
+		assertEquals(LocalDate.of(2020, 1, 13), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 8), 6));
+		assertEquals(LocalDate.of(2020, 1, 14), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 8), 7));
+		// 土曜日を起点として翌営業日を算出。
+		assertEquals(LocalDate.of(2020, 1, 11), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 11), 1));
+		assertEquals(LocalDate.of(2020, 1, 12), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 11), 2));
+		assertEquals(LocalDate.of(2020, 1, 13), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 11), 3));
+	}
+
+	@Test
+	public void testOffSpecific() {
+		// regularOn: 月火水木金。
+		// specificOff: 2019/01/20(月), 2019/01/21(火), 2019/01/22(水), 2019/01/23(木), 2019/01/24(金)
+		FileWorkdayStrategy impl = new FileWorkdayStrategy(new File("src/test/resources/workday"));
+		// 水曜日を起点として営業日数を算出。
+		assertEquals(1, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 15)));
+		assertEquals(2, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 16)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 17)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 18)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 19)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 20)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 21)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 22)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 23)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 24)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 25)));
+		assertEquals(3, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 26)));
+		assertEquals(4, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 27)));
+		assertEquals(5, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 15), LocalDate.of(2020, 1, 28)));
+		// 土曜日を起点として営業日数を算出。
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 18)));
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 19)));
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 20)));
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 21)));
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 22)));
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 23)));
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 24)));
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 25)));
+		assertEquals(0, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 26)));
+		assertEquals(1, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 27)));
+		assertEquals(2, impl.getNumberOfWorkday("standard", LocalDate.of(2020, 1, 18), LocalDate.of(2020, 1, 28)));
+		// 水曜日を起点として翌営業日を算出。
+		assertEquals(LocalDate.of(2020, 1, 15), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 15), 1));
+		assertEquals(LocalDate.of(2020, 1, 16), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 15), 2));
+		assertEquals(LocalDate.of(2020, 1, 17), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 15), 3));
+		assertEquals(LocalDate.of(2020, 1, 27), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 15), 4));
+		assertEquals(LocalDate.of(2020, 1, 28), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 15), 5));
+		// 土曜日を起点として翌営業日を算出。
+		assertEquals(LocalDate.of(2020, 1, 18), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 18), 0));
+		assertEquals(LocalDate.of(2020, 1, 27), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 18), 1));
+		assertEquals(LocalDate.of(2020, 1, 28), impl.getNextWorkday("standard", LocalDate.of(2020, 1, 18), 2));
+	}
+
+}
